c2737c27552cba48596718f26e31c7333b133457
guilhemvnt <vinet.guilhem@outlook.com>
2025-04-27 23:41:19 +0100

    [ADD] clean input handle

--- include/Client/Game.hpp
+++ include/Client/Game.hpp
@@ -16,6 +16,11 @@
 #include <string>
 #include <NetworkClient.hpp>
 
+#include <optional>
+#include <utility>
+
+
+
 enum class GameState {
     STATE_DEFAULT,
     STATE_MENU,
@@ -28,7 +33,8 @@ public:
     ~Game();
 
     void run();
-    void inputHandling();
+    void inputHandling(const std::optional<sf::Event>& event);
+
 
     void parseClientInput(const std::string &data);
     void processToken(const std::string &token);
@@ -46,17 +52,24 @@ private:
 
     std::string assetsPath;
     NetworkClient _client;
-
-    bool _upPressed;
-    bool _downPressed;
-    bool _leftPressed;
-    bool _rightPressed;
-
     std::string _assetsPath;
 
     GameState _currentState;
     std::map<GameState, std::shared_ptr<IScene>> _scenes;
     std::map<std::string, sf::Texture> _textures;
+
+
+    struct KeyAction {
+        std::function<void()> action;
+        bool isPressed;
+    };
+    
+    std::map<sf::Keyboard::Key, KeyAction> keyMappings = {
+        {sf::Keyboard::Key::Z, { [&]() { _client.sendPacket("Up_P"); }, false }},
+        {sf::Keyboard::Key::S, { [&]() { _client.sendPacket("Do_P"); }, false }},
+        {sf::Keyboard::Key::D, { [&]() { _client.sendPacket("Ri_P"); }, false }},
+        {sf::Keyboard::Key::Q, { [&]() { _client.sendPacket("Le_P"); }, false }},
+    };   
 };
 
 #endif /* !GAME_HPP_ */
